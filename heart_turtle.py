b130dc8b9c90d49b1936f147418a974378212eb9
lallox <dliaadam527@gmail.com>
2024-11-10 00:56:49 +0100

    heart_turtle.py

--- heart_turtle.py
+++ heart_turtle.py
@@ -10,7 +10,7 @@ pen = turtle.Turtle()
 pen.speed(0) 
 pen.color("red")
 
-#functiuon to define the haert shape
+#functiuon to define the heart shape
 def heart(t):
     x = 16 * math.sin(t) ** 3
     y = 13 * math.cos(t) - 5 * math.cos(2*t) - 2 * math.cos(3*t) - math.cos(4*t)
